67a7ab9465f20cd58c030f3809e180dcec9974f9
ChenJunXu-Debug <1348901816@qq.com>
2019-08-03 11:00:28 +0800

    test commit

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Test
+ * Author:   ASUS
+ * Date:     2019/8/2 21:05
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈测试〉
+ *
+ * @author ASUS
+ * @create 2019/8/2
+ * @since 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
